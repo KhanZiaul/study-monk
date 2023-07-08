bdcbe60f499f90b5fc371efd3ab746c317aa709e
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-07-08 13:53:25 +0600

    login-register done

--- js/loginRegister.js
+++ js/loginRegister.js
@@ -1,6 +1,12 @@
 const login = document.getElementById('login-container');
-const register = document.getElementById('login-container');
+const register = document.getElementById('register-container');
 
 document.getElementById('login-link').addEventListener("click",function(){
     login.style.display = 'none';
+    register.style.display = 'block';
+})
+
+document.getElementById('register-link').addEventListener("click",function(){
+    register.style.display = 'none';
+    login.style.display = 'block';
 })
